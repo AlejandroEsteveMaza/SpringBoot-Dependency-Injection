18553320fdfc9faaf401e7af5277eb6a61b481a4
AlejandroEsteveMaza <alex.esteve98@gmail.com>
2023-03-29 23:26:48 +0200

    Registrar items factura

--- src/main/java/com/springboot/di/app/AppConfig.java
+++ src/main/java/com/springboot/di/app/AppConfig.java
@@ -1,9 +1,14 @@
 package com.springboot.di.app;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import com.springboot.di.app.models.domain.ItemFactura;
+import com.springboot.di.app.models.domain.Producto;
 import com.springboot.di.app.models.service.IServicio;
 import com.springboot.di.app.models.service.MiServicio;
 import com.springboot.di.app.models.service.MiServicioComplejo;
@@ -15,13 +20,22 @@ public class AppConfig {
 	@Primary
 	public IServicio registrarMiServicio() {
 		return new MiServicio();
-		
 	}
-	
+
 	@Bean("miServicioComplejo")
 	public IServicio registrarMiServicioComplejo() {
 		return new MiServicioComplejo();
-		
 	}
-	
+
+	@Bean("itemsFactura")
+	public List<ItemFactura> registrarItems() {
+		Producto producto1 = new Producto("Camara sony", 100);
+		Producto producto2 = new Producto("Bicicleta Bianchi 20k2", 200);
+
+		ItemFactura linea1 = new ItemFactura(producto1, 2);
+		ItemFactura linea2 = new ItemFactura(producto2, 4);
+
+		return Arrays.asList(linea1, linea2);
+	}
+
 }
